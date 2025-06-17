e98202532d27ca1f6bdf25a111dd3e3eb8bdf52d
Szymon Turno <szymon@turno.pl>
2025-06-18 01:18:52 +0200

    feat: add build targets

--- test/iso/tests.mk
+++ test/iso/tests.mk
@@ -1,6 +1,11 @@
-run-iso_tests:
-	make _run-tests cantil_YAMLS="$(test_DIR)/iso/cantil.yaml \
-		$(cantil_YAMLS)" test_POSIX_EN="false" BLDDIR="build-iso_tests"
+run-iso_tests: build-iso_tests
+	make _run-tests BLDDIR="build-iso_tests"
+
+build-iso_tests:
+	make build-iso_tests/$(test_DIR)/app \
+		cantil_YAMLS="$(test_DIR)/iso/cantil.yaml $(cantil_YAMLS)" \
+		test_MULTITHREADING_EN="false" \
+		BLDDIR="build-iso_tests"
 
 coverage-iso_tests:
 	make _coverage BLDDIR="build-iso_tests"
--- test/posix/tests.mk
+++ test/posix/tests.mk
@@ -1,6 +1,11 @@
-run-posix_tests:
-	make _run-tests cantil_YAMLS="$(test_DIR)/posix/cantil.yaml \
-		$(cantil_YAMLS)" test_MULTITHREADING_EN="true" BLDDIR="build-posix_tests"
+run-posix_tests: build-posix_tests
+	make _run-tests BLDDIR="build-posix_tests"
+
+build-posix_tests:
+	make build-posix_tests/$(test_DIR)/app \
+		cantil_YAMLS="$(test_DIR)/posix/cantil.yaml $(cantil_YAMLS)" \
+		test_MULTITHREADING_EN="true" \
+		BLDDIR="build-posix_tests"
 
 coverage-posix_tests:
 	make _coverage BLDDIR="build-posix_tests"
