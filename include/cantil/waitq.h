983989f3d75dece2d9e70eca9875a76fbe32fd10
Szymon Turno <szymon@turno.pl>
2025-07-19 16:32:04 +0200

    feat: add more docsy

--- include/cantil/waitq.h
+++ include/cantil/waitq.h
@@ -8,14 +8,19 @@
 
 #include "cn/waitq.h"
 
+/** @see cn_waitq_create() */
 #define waitq_create cn_waitq_create
 
+/** @see cn_waitq_destroy() */
 #define waitq_destroy cn_waitq_destroy
 
+/** @see cn_waitq_ins() */
 #define waitq_ins cn_waitq_ins
 
+/** @see cn_waitq_rem() */
 #define waitq_rem cn_waitq_rem
 
+/** @see cn_waitq_tryrem() */
 #define waitq_tryrem cn_waitq_tryrem
 
 #endif /* CANTIL_WAITQ_H */
--- include/cn/waitq.h
+++ include/cn/waitq.h
@@ -10,16 +10,48 @@
 
 #include "cn/cirq.h"
 
+/**
+ * @var typedef struct CnWaitq CnWaitq
+ * @brief *** todo ***.
+ */
 typedef struct CnWaitq CnWaitq;
 
+/**
+ * @fn CnWaitq* cn_waitq_create(void)
+ * @brief *** todo ***.
+ * @returns *** todo ***.
+ */
 CnWaitq* cn_waitq_create(void);
 
+/**
+ * @fn void cn_waitq_destroy(CnWaitq* waitq)
+ * @brief *** todo ***.
+ * @param[in,out] waitq Input/output.
+ */
 void cn_waitq_destroy(CnWaitq* waitq);
 
+/**
+ * @fn void cn_waitq_ins(CnWaitq* waitq, struct CnBinode* entry)
+ * @brief *** todo ***.
+ * @param[in,out] waitq Input/output.
+ * @param[in,out] entry Input/output.
+ */
 void cn_waitq_ins(CnWaitq* waitq, struct CnBinode* entry);
 
+/**
+ * @fn struct CnBinode* cn_waitq_rem(CnWaitq* waitq)
+ * @brief *** todo ***.
+ * @param[in,out] waitq Input/output.
+ * @returns *** todo ***.
+ */
 struct CnBinode* cn_waitq_rem(CnWaitq* waitq);
 
+/**
+ * @fn struct CnBinode* cn_waitq_tryrem(CnWaitq* waitq)
+ * @brief *** todo ***.
+ * @param[in,out] waitq Input/output.
+ * @returns *** todo ***.
+ */
 struct CnBinode* cn_waitq_tryrem(CnWaitq* waitq);
 
 #endif /* CN_WAITQ_H */
