d85c6155dcbc95393c19a01556d0428eb275f898
Szymon Turno <szymon@turno.pl>
2025-08-03 21:17:54 +0200

    feat: add doxy for waiting queue

--- include/cn/waitq.h
+++ include/cn/waitq.h
@@ -11,45 +11,63 @@
 
 /**
  * @var typedef struct CnWaitq CnWaitq
- * @brief *** todo ***.
+ *
+ * @brief Waiting queue.
  */
 typedef struct CnWaitq CnWaitq;
 
 /**
  * @fn CnWaitq* cn_waitq_create(void)
- * @brief *** todo ***.
- * @return *** todo ***.
+ *
+ * @brief Create a new waiting queue.
+ *
+ * @return The new waiting queue.
  */
 CnWaitq* cn_waitq_create(void);
 
 /**
  * @fn void cn_waitq_destroy(CnWaitq* waitq)
- * @brief *** todo ***.
- * @param[in,out] waitq Input/output.
+ *
+ * @brief Destroy a waiting queue.
+ *
+ * @param[in,out] waitq The waiting queue.
  */
 void cn_waitq_destroy(CnWaitq* waitq);
 
 /**
  * @fn void cn_waitq_ins(CnWaitq* waitq, struct CnBinode* entry)
- * @brief *** todo ***.
- * @param[in,out] waitq Input/output.
- * @param[in,out] entry Input/output.
+ *
+ * @brief Insert an entry into a waiting queue
+ *.
+ * @param[in,out] waitq The waiting queue.
+ * @param[in,out] entry The inserted entry.
  */
 void cn_waitq_ins(CnWaitq* waitq, struct CnBinode* entry);
 
 /**
  * @fn struct CnBinode* cn_waitq_rem(CnWaitq* waitq)
- * @brief *** todo ***.
- * @param[in,out] waitq Input/output.
- * @return *** todo ***.
+ *
+ * @brief Remove an entry from the front of the queue.
+ *
+ * @param[in,out] waitq The waiting queue.
+ *
+ * This will block the thread if the queue is empty and wake it up only after an
+ * entry has been inserted.
+ *
+ * @return The removed entry.
  */
 struct CnBinode* cn_waitq_rem(CnWaitq* waitq);
 
 /**
  * @fn struct CnBinode* cn_waitq_tryrem(CnWaitq* waitq)
- * @brief *** todo ***.
- * @param[in,out] waitq Input/output.
- * @return *** todo ***.
+ *
+ * @brief Try to remove an entry from the front of the queue.
+ *
+ * @param[in,out] waitq The waiting queue.
+ *
+ * This will not block the thread.
+ *
+ * @return The removed entry if the queue was not empty. Otherwise, NULL.
  */
 struct CnBinode* cn_waitq_tryrem(CnWaitq* waitq);
 
