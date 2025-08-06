3686ec1676b036aeaf263ae12d0763ffaaf3b822
Szymon Turno <szymon@turno.pl>
2025-08-06 23:51:44 +0200

    fix: rename streambag

--- include/cn/logger/streambag.h
+++ include/cn/logger/streambag.h
@@ -41,52 +41,52 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "cn/os/fstream.h"
 
 /**
- * @var typedef struct CnStreambag CnStreambag
+ * @var typedef struct CnStreamBag CnStreamBag
  *
  * @brief A bag of streams.
  */
-typedef struct CnStreambag CnStreambag;
+typedef struct CnStreamBag CnStreamBag;
 
 /**
- * @fn CnStreambag* cn_streambag_create(void)
+ * @fn CnStreamBag* cn_streambag_create(void)
  *
  * @brief Create a new empty bag of streams.
  *
  * @return An empty bag of streams.
  */
-CnStreambag* cn_streambag_create(void);
+CnStreamBag* cn_streambag_create(void);
 
 /**
- * @fn void cn_streambag_destroy(CnStreambag* bag)
+ * @fn void cn_streambag_destroy(CnStreamBag* bag)
  *
  * @brief Destroy a bag of streams
  *
  * @param[in,out] bag The bag.
  */
-void cn_streambag_destroy(CnStreambag* bag);
+void cn_streambag_destroy(CnStreamBag* bag);
 
 /**
- * @fn void cn_streambag_ins(CnStreambag* bag, CnFstream* stream)
+ * @fn void cn_streambag_ins(CnStreamBag* bag, CnFstream* stream)
  *
  * @brief Insert a stream into a bag.
  *
  * @param[in,out] bag The bag.
  * @param[in] stream The stream.
  */
-void cn_streambag_ins(CnStreambag* bag, CnFstream* stream);
+void cn_streambag_ins(CnStreamBag* bag, CnFstream* stream);
 
 /**
- * @fn void cn_streambag_rem(CnStreambag* bag, CnFstream* stream)
+ * @fn void cn_streambag_rem(CnStreamBag* bag, CnFstream* stream)
  *
  * @brief Remove a stream from a bag.
  *
  * @param[in,out] bag The bag.
  * @param[in] stream The stream.
  */
-void cn_streambag_rem(CnStreambag* bag, CnFstream* stream);
+void cn_streambag_rem(CnStreamBag* bag, CnFstream* stream);
 
 /**
- * @fn void cn_streambag_vprint(CnStreambag* bag, const char* format, va_list vlist)
+ * @fn void cn_streambag_vprint(CnStreamBag* bag, const char* format, va_list vlist)
  *
  * @brief Print to all streams from a bag.
  *
@@ -94,6 +94,6 @@ void cn_streambag_rem(CnStreambag* bag, CnFstream* stream);
  * @param[in] format The format string.
  * @param[in] vlist The list of arguments.
  */
-void cn_streambag_vprint(CnStreambag* bag, const char* format, va_list vlist);
+void cn_streambag_vprint(CnStreamBag* bag, const char* format, va_list vlist);
 
 #endif /* CN_LOGGER_STREAMBAG_H */
